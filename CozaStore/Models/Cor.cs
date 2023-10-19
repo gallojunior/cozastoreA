d40167a46a0e274034119dacb25fdac53f153365
GalloJunior <gallojunior@gmail.com>
2023-10-19 09:46:43 -0300

    Criando a camada de dados

---
+++ CozaStore/Models/Cor.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CozaStore.Models;
+
+[Table("Cor")]
+public class Cor
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public byte Id { get; set; }
+
+    [Required(ErrorMessage = "Informe o Nome")]
+    [StringLength(30, ErrorMessage = "O Nome deve possuir no máximo 30 caracteres")]
+    public string Nome { get; set; }
+
+    [Display(Name = "Código da Cor")]
+    [Required(ErrorMessage = "Informe o Código da Cor")]
+    [StringLength(7, ErrorMessage = "O Código da Cor deve possuir no máximo 7 caracteres")]
+    public string CodigoHexa { get; set; }
+}
